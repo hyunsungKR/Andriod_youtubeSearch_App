63fc798ce2032a3902f8b7085054b99d24fa2719
WangHyunSung <120348500+hyunsungKR@users.noreply.github.com>
2023-04-05 09:44:09 +0900

    Delete Config.java

--- app/src/main/java/com/hyunsungkr/youtubeapp/config/Config.java
+++
@@ -1,8 +0,0 @@
-package com.hyunsungkr.youtubeapp.config;
-
-public class Config {
-
-    public static final String BASE_URL = "https://www.googleapis.com";
-    public static final String PATH = "/youtube/v3/search";
-    public static final String API_KEY = "AIzaSyDCSKVc8CTTE2u0Ej37K3MHVirLuUd038Q";
-}
